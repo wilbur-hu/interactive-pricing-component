5313a256d95d415abf40fb0f55cbcce92d036a6b
Wilbur Hu <wilburhu83@gmail.com>
2024-03-15 20:20:28 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import PriceCard from "@/components/PriceCard.tsx";
 function App() {
   return (
     <div className="font-manrope flex h-[800px] w-[1440px] shrink-0 flex-col items-center bg-white bg-[url('/bg-pattern.svg')] bg-contain bg-no-repeat">
-      <Header className="mt-[67px]" />
+      <Header className="mt-[104px]" />
       <PriceCard className="mt-[92px]" />
     </div>
   );
